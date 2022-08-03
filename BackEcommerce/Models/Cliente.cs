5bcb42446b2e64000e6695a0c9372a779c0a1ce7
Misael MT <misael_mootun@outlook.com>
2022-08-03 15:03:21 -0500

    Paginacion cliente

--- BackEcommerce/Models/Cliente.cs
+++ BackEcommerce/Models/Cliente.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BackEcommerce.Models
 {
-    public partial class Cliente
+    public partial class Cliente 
     {
         public Cliente()
         {
@@ -12,16 +12,17 @@ namespace BackEcommerce.Models
         }
 
         public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Apellidos { get; set; }
-        public string Telefono { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellidos { get; set; }
+        public string? Telefono { get; set; }
         
-        public string Correo { get; set; }
+        public string? Correo { get; set; }
         public string? Estatus { get; set; } = null!;
      
         public DateTime? FechaCreate { get; set; }
         public DateTime? FechaDelete { get; set; }
 
         public virtual ICollection<Pedido> Pedidos { get; set; }
+
     }
 }
